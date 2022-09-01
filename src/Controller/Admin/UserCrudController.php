cbdb5cfd9331c94019d15a09e93922abaeeda849
Redo76 <redouane.hamadache1@gmail.com>
2022-09-01 17:14:15 +0200

    Ajout CRUD Admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +17,37 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+            ->setDateFormat('...')
+            // ...
+            ->setPageTitle('index', 'To do list - Administration des utilisateurs')
+            ->setPaginatorPageSize(10)
+        ;
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('email')
+                ->hideOnForm()
+                ->setFormTypeOption('disabled','disabled'),
+            TextField::new('username'),
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            ArrayField::new('roles'),
+            TextField::new('googleId')
+                ->hideOnForm(),
+            TextField::new('githubId')
+                ->hideOnForm(),
+
         ];
     }
-    */
+
 }
